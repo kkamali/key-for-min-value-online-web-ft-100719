4b5bd433a07fb359cb0ea0991a9b086ac6276c4e
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-10 02:38:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,9 @@ def key_for_min_value(name_hash)
   lowest_value = "bloop"
   lowest_key = "bleep"
   name_hash.each do |key, value|
-    
+    if lowest_value = "bloop"
+      lowest_value = value 
+      lowest_key = key 
+    end
   end 
 end
